5fa4331bef0b9044444952476a26a7c7970a1af5
robertcnagy <robertc.nagy@nagyseo.com>
2024-02-04 22:12:41 +0000

    Added more pages

--- app/views/registration/current/routes/routes.js
+++ app/views/registration/current/routes/routes.js
@@ -8,16 +8,50 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 // Q: Registrant type
-
-
-  router.post('/registrant-type-answer', function(request, response) {
+router.post('/registrant-type-answer', function(request, response) {
 
     var registrantType = request.session.data['registrant-type']
     if (registrantType == "none"){
-        response.redirect("/registrant-type-fail")
+        response.redirect("registrant-type-fail")
     } else {
-        response.redirect("/registrant-type")
+        response.redirect("registrant")
     }
 })
+
+// Q: Registrant
+router.post('/registrant-answer', function(request, response) {
+
+    var registrantType = request.session.data['registrant-type']
+    if (registrantType == "central"){
+        response.redirect("exemption")
+    } else {
+        response.redirect("written-permission")
+    }
+})
+
+// Q: Exemption
+router.post('/exemption-answer', function(request, response) {
+
+    var exemptionRt = request.session.data['exemption']
+    if (exemptionRt == "no"){
+        response.redirect("exemption-fail")
+    } else {
+        response.redirect("exemption-upload")
+    }
+})
+
+// Q: Written permission
+router.post('/written-permission-answer', function(request, response) {
+
+    var writtenPermission = request.session.data['written-permission']
+    if (writtenPermission == "yes"){
+        response.redirect("written-permission-upload")
+    } else {
+        response.redirect("written-permission-fail")
+    }
+})
+
+
+
 module.exports = router
    
