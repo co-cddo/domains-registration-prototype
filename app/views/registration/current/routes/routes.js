f1374a91466004af17e77db77c6220b9f33e86ff
Robert Nagy <robertc.nagy@nagyseo.com>
2025-03-06 11:51:06 +0000

    Registrant type page

--- app/views/registration/current/routes/routes.js
+++ app/views/registration/current/routes/routes.js
@@ -19,7 +19,7 @@ router.post('/registrant-type-answer', function(request, response) {
     if (registrantType == "none"){
         response.redirect("registrant-type-fail")}
     // Q: Route 1
-        else if (registrantType == "Parish or community council" || registrantType == "Town" ){
+        else if (registrantType == "Parish or community council" ){
             response.redirect("domain")}
     // Q: Route 2
     else if (registrantType == "Central government department or agency" || registrantType == "Non-departmental body - also known as an arm's length body" ){
